a04e60097342fe658111c70f7a2f4f28d8acae2b
Pedro Skakum <pedroskakum@gmail.com>
2025-06-19 21:02:54 -0300

    webview implementation

---
+++ app/src/main/java/com/example/helloworldwebviewapplication/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.helloworldwebviewapplication
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val webView = WebView(this).apply {
+            settings.apply {
+                javaScriptEnabled = true
+                domStorageEnabled = true
+                loadWithOverviewMode = true
+                useWideViewPort = true
+                cacheMode = WebSettings.LOAD_DEFAULT
+                setSupportZoom(false)
+                builtInZoomControls = false
+                displayZoomControls = false
+            }
+
+            webViewClient = WebViewClient()
+
+            loadUrl("https://google.com.br/")
+        }
+
+        setContentView(webView)
+
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (webView.canGoBack()) {
+                    webView.goBack()
+                } else {
+                    isEnabled = false
+                    onBackPressedDispatcher.onBackPressed()
+                }
+            }
+        })
+    }
+}
+
